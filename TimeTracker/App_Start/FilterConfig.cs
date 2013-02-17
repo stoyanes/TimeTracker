d703f4021f6f27f255cc0355b98ef899baf0468e
Stoyan Stoyanov <djstoxer@gmail.com>
2013-02-17 16:25:30 +0200

    changed connection string

--- TimeTracker/App_Start/FilterConfig.cs
+++ TimeTracker/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace TimeTracker
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
